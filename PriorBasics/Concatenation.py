05d47271c23421c97e240d1b036daa09d47c3256
RavitejaPalakurthi <rajaraviteja33@gmail.com>
2026-06-06 14:50:16 +0530

    Adding concatenation

---
+++ PriorBasics/Concatenation.py
@@ -0,0 +1,20 @@
+print(10+10)
+print(10.5+10.5)
+print(10+10.5)
+print("Raviteja"+"Palakurthi")
+#print("Raviteja"+10)
+# 20
+# 21.0
+# 20.5
+# RavitejaPalakurthi
+# TypeError: can only concatenate str (not "int") to str
+print(True+10)
+print(True+True)
+print(True+False)
+print(True+False+True)
+# 11
+# 2
+# 1
+# 2
+print(True+"Raviteja")
+# TypeError: unsupported operand type(s) for +: 'bool' and 'str'
