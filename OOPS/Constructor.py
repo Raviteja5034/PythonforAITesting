47a8cbd212e9ebe6f84c767080303a7dc3129058
RavitejaPalakurthi <rajaraviteja33@gmail.com>
2026-06-13 15:23:40 +0530

    Constructor

---
+++ OOPS/Constructor.py
@@ -0,0 +1,9 @@
+class TestClass:
+    def __init__(self,name):
+        print("my name is",name)
+
+    def testmethod1(self,sport):
+        print("My fav sport is",sport)
+
+obj6=TestClass("Raviteja") #my name is Raviteja
+obj6.testmethod1("Hockey") #My fav sport is Hockey
