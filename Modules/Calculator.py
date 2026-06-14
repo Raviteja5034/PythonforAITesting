bf2be306768d9fffe63042fe24f240386637d55c
RavitejaPalakurthi <rajaraviteja33@gmail.com>
2026-06-14 22:42:44 +0530

    Oops concepts

---
+++ Modules/Calculator.py
@@ -0,0 +1,5 @@
+def add(num1,num2):
+    print("Sum is:",num1+num2)
+
+def mul(num1,num2):
+    print("Mul is:",num1*num2)
---
+++ Modules/Operations.py
@@ -0,0 +1,18 @@
+#Approach 1
+import Calculator
+Calculator.add(10,20)
+Calculator.mul(10,20)
+
+# Sum is: 30
+# Mul is: 200
+
+#Approach2
+from Calculator import add,mul
+add(10,20)
+mul(10,20)
+#
+
+#Approach 3
+from Calculator import *
+add(20,20)
+mul(30,40)
